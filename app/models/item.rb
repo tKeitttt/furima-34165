9490f0e6b6bf2330d458beb27810039e3bd08b8f
tKeitttt <oakeisuke0308@gmail.com>
2021-04-23 18:05:09 +0900

    価格に範囲と半角数字のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   with_options presence: true do    
     validates :item_name
     validates :item_text
-    validates :price
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :item_image
 
     with_options numericality: { other_than:1 } do
