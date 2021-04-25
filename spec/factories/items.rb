0918e73efb6644a12ec1dfa32caa72034d5f971a
tKeitttt <oakeisuke0308@gmail.com>
2021-04-25 12:37:21 +0900

    Itemモデル単体テストファイルの準備とFactoryBotでのインスタンス生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    item_text {Faker::Lorem.sentence}
+    item_image {Faker::Lorem.sentence}
+    item_name {Faker::Lorem.sentence}
+    category_id {rand(2..11)}
+    status_id {rand(2..7)}
+    delivery_fee_id {rand(2..3)}
+    prefecture_id {rand(2..48)}
+    shipment_date_id {rand(2..4)}
+    price { rand(300..9999999) }
+    association :user 
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品情報の保存' do
+    context '商品が登録できる場合' do
+      it '商品名、商品説明、商品画像、カテゴリ、状態、配送料、発送元、発送までの日数、価格の入力があれば投稿できる' do
+      end
+    end
+    context '商品が登録できない場合' do
+      it '商品名が空では投稿できない' do
+      end     
+    end
+  end
 end
