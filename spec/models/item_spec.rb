a9c98b232708c76882449141c98fa19f411eca9d
tKeitttt <oakeisuke0308@gmail.com>
2021-04-26 12:37:15 +0900

    コードレビューコメント修正2回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -93,6 +93,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
       end
+      it '価格が半角英数混合では投稿できない' do
+        @item.price="abc123"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it '価格が半角英語では投稿できない' do
+        @item.price="abc"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
       it '価格が300未満では投稿できない' do
         @item.price=rand(1..299)
         @item.valid?
