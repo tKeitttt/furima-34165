fd4ae64fbfe0e07abf2299aed987f21f21691cc5
tKeitttt <oakeisuke0308@gmail.com>
2021-04-30 21:06:56 +0900

    フォームの実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,7 +7,11 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city, :address
-    validates :phone_number, format: {with: /^0\d{9,10}$/}
+    validates :phone_number
   end
 
+  def save
+    order=Order.create(item:item, user:user)
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
