2b681e177734cd888e079e6e4b54aac9cb5d2ea6
tKeitttt <oakeisuke0308@gmail.com>
2021-05-07 21:01:51 +0900

    売却済み商品購入画面へのアクセス防止

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if (current_user.id == @item.user_id) || !@item.order.nil?
     @order_address = OrderAddress.new
   end
 
