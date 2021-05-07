d0cb594b8522ed28cf709021d5f3370f926ee1ee
tKeitttt <oakeisuke0308@gmail.com>
2021-05-07 20:33:50 +0900

    モデル単体テストコード実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,7 +7,7 @@ class OrderAddress
     validates :item_id
     validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1}
     validates :city, :address
     validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ }
   end
